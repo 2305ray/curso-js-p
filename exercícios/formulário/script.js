93be8b630e61b0dbaa11946a915dc4e449229029
2305ray <rayssagarcia035@gmail.com>
2024-10-18 10:22:19 -0300

    formulario

--- exercícios/formulário/script.js
+++ exercícios/formulário/script.js
@@ -1,22 +1,33 @@
 //não atualizar a página
 function meuEscopo () {
     const form = document.querySelector('.form');
-/*
-    form.onsubmit = function (evento) {
-        alert(1);
-        console.log('Foi enviado.');
-    };
-*/
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
 
 function recebeEventoForm (evento) {
-    event.preventDefault();
+    evento.preventDefault();
+
     const nome = form.querySelector('.nome');
     const sobrenome = form.querySelector('.sobrenome');
     const peso = form.querySelector('.peso');
     const altura = form.querySelector('.altura');
+
+    pessoas.push ({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value,
+
+    }); // criação de um objeto q vai dentro do array pessoas
+
+    console.log(pessoas);
+
+    resultado.innerHTML += `<p>Nome: ${nome.value} ${sobrenome.value}</p>
+                                <p>Peso: ${peso.value}</p>
+                                <p>Altura: ${altura.value}</p>`;
 }
     form.addEventListener('submit', recebeEventoForm);
 };
-
-
  meuEscopo();
