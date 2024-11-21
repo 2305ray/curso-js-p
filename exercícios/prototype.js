246d1edfca15a39a62f459ec40db822f96a49760
2305ray <rayssagarcia035@gmail.com>
2024-11-21 12:34:53 -0300

    proto

--- exercícios/prototype.js
+++ exercícios/prototype.js
@@ -1,13 +1,21 @@
 function Pessoa(nome, sobrenome) {
     this.nome = nome;
     this.sobrenome = sobrenome;
-    this.nomeCompleto= () => this.nome + ' ' + thid.sobrenome;
+    this.nomeCompleto= () => 'ORIGINAL: ' + this.nome + ' ' + this.sobrenome;
 }
 
 Pessoa.prototype.estouAqui= 'hahahahah';
+//pessoa.prototype === pesoa1._proto_
+
+pessoa1.prototype.nomeCompleto = function() {
+    return this.nome + ' ' + this.sobrenome;
+};
 
 const pessoa1 = new Pessoa('Luiz', 'otavio'); // pessoa = função construtora
 const pessoa2 = new Pessoa('Carol', 'garcia'); // pessoa = função construtora
 
 console.dir(pessoa1);
 console.dir(pessoa2);
+
+
+
